d191e5b3295d217f28006da67ad8e61eaf6be710
Thanh Thien <thanhthien160501@gmail.com>
2024-12-30 11:42:29 +0700

    heder #2

---
+++ src/components/Layout/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from "classnames/bind";
+import styles from "./AccountItem.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+  return (
+    <div className={cx("wrapper")}>
+      <img
+        className={cx("avatar")}
+        src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/a187c4dfa896a5ea449a4c5d3927b20a~c5_300x300.webp?lk3s=a5d48078&nonce=77098&refresh_token=8e8f18c5d1664f748421a7d1e7ef8a31&x-expires=1735455600&x-signature=ubxwdZqr4iDbEUGOUaJsfVs9aB4%3D&shp=a5d48078&shcp=c1333099"
+        alt="Hoa"
+      />
+      <div className={cx("info")}>
+        <h4 className={cx("name")}>
+          <span>Nguyen Van A</span>
+          <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+        </h4>
+        <span className={cx("username")}>nguyenvana</span>
+      </div>
+    </div>
+  );
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,36 +1,73 @@
 import classNames from "classnames/bind";
+import { useState, useEffect } from "react";
 import styles from "./Header.module.scss";
-import images from '~/assets/images';
+import images from "~/assets/images";
+
+import Button from "~/components/Button";
+import { Wrapper as PopperWrapper } from "~/components/Popper";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircleXmark,
+  faSpinner,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
-
+import Tippy from "@tippyjs/react/headless";
+import AccountItem from "../AccountItem";
 
 const cx = classNames.bind(styles);
 
 function Header() {
-  console.log(faCircleXmark);
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      setSearchResult([]);
+    }, 0);
+  });
+
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
         <div className={cx("logo")}>
-          <img scr={images.logo} alt='TikTok'/>
+          <img scr={images.logo} alt="TikTok" />
         </div>
 
-        <div className={cx('search')}>
-          <input placeholder="Search..." spellCheck={false}/>
-          <button className={cx('clear')}>
-          <FontAwesomeIcon icon={faCircleXmark} />
-          
-          </button>
-          {/* Loading */}
-          <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-          <button className={cx('search-btn')}>
-            {/* Search */}
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </button>
+        <Tippy
+          interactive
+          visible={searchResult.length > 0}
+          render={(attrs) => (
+            <div className={cx("search-result")} tabIndex="-1" {...attrs}>
+              <PopperWrapper>
+                <h4 className={cx("search-title")}>Accounts</h4>
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+                <AccountItem />
+              </PopperWrapper>
+            </div>
+          )}
+        >
+          <div className={cx("search")}>
+            <input placeholder="Search..." spellCheck={false} />
+            <button className={cx("clear")}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+            {/* Loading */}
+            <FontAwesomeIcon className={cx("loading")} icon={faSpinner} />
+
+            {/* Tippy */}
+
+            <button className={cx("search-btn")}>
+              {/* Search */}
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </button>
+          </div>
+        </Tippy>
+        <div className={cx("actions")}>
+          <Button primary onClick={() => alert("Click login")}>
+            Log in
+          </Button>
         </div>
-        <div className={cx('actions')}></div>
       </div>
     </header>
   );
